f910a46e2cbf69b704eaa5c5ff7cf78eb4faecad
schamaxi@gmail.com <schamaxi@gmail.com>
2026-05-17 13:32:10 +0000

    update

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -3,18 +3,26 @@ with orders as(
 select * from {{ref('stg_jaffle_shop__orders')}}
 
 ),
-with customers as(
-
-  select * from {{ref('dim_customers')}} 
-),
 with payments as(
-   select * from {{ref('stg_stripe_payments')}}   
+   select * from {{ ref ('stg_stripe__payment') }}
+),
+order_payments as (
+    select
+        order_id,
+        sum (case when payment_status = 'success' then payment_amount end) as amount
+    from payments
+    group by 1
+),
+ final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
-select orders.order_id,
-orders.customer_id,
-payments.amount
-from payments paym
-inner join orders ords
-    ON paym.order_id=ords.order_id
-inner join customers cust
-    ON paym.customer_id=cust.customer_id    
+
+select * from final
